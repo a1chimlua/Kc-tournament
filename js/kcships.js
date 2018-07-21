76a68ac9a3ae28404f35ea6f4909a58f84efc077
a1chimlua <39865672+a1chimlua@users.noreply.github.com>
2018-07-22 01:06:51 +0700

    Update kcships.js

--- js/kcships.js
+++ js/kcships.js
@@ -552,7 +552,7 @@ Ship.prototype.weightedAntiAir = function() {
 
 Ship.prototype.getAACItype = function(atypes) {
 	var types = [];
-	if (this.side == 1) return types; //enemy can't do AACI?
+	if (this.side >= 1) return types; //enemy can't do AACI?
 	
 	var concentrated = false;
 	for (var i=0; i<this.equips.length; i++) {
